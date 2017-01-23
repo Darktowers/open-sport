bc07c89ce114243bc8c4361b2ea6c7b05bb96a9a
Eliana Prada <darktowerdev@gmail.com>
2017-01-23 17:39:29 -0500

    Terminando ajustes perfil

--- profile.php
+++ profile.php
@@ -1,4 +1,74 @@
-<?php include("header.php");?>
+<!DOCTYPE html>
+<html lang="es" xml:lang="es">
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Open Sport</title>
+	<meta name="keywords" content="">
+	<meta name="author" content="">
+	<meta name="description" content="">
+	<meta name="viewport" content="width=device-width, user-scalable=yes, initial-scale=1.0, minimum-scale=1.0">
+	<link rel="icon" href="favicon.ico">
+	<link rel="stylesheet" href="css/main.css">
+	<link rel="stylesheet" href="css/other-styles.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+	<div class="notification">
+		<img src="img/transparent-rectangles-small.png" alt="" class="mosaico">
+		<div class="wrap slidex">
+			<div class="notifi">
+				
+				<p><b>EN VIVO:</b> Barça vs Real Madrid, desde el Estadio Santiago Bernabéu</p>
+				<a href="" class="btn btn-b_black">VER</a>
+			</div>
+			<div class="notifi">
+		
+				<p><b>EN VIVO:</b> Atletico Madrid vs Galicia desde el Estadio Santiago Bernabéu</p>
+				<a href="" class="btn btn-b_black">VER</a>
+			</div>
+			<div class="notifi">
+				
+				<p><b>EN VIVO:</b> Formula 1 G.P. MALASIA</p>
+				<a href="" class="btn btn-b_black">VER</a>
+			</div>
+		</div>
+	</div>
+	<header class="header">
+
+		<div class="header-Container wrap">
+			<a href="index.php">
+			<div class="header-Container-Logo">
+				<img src="img/logo-open-sport.svg" alt="">
+			</div>
+			</a>
+			<div class="menu-icon">
+				<i class="fa fa-bars" aria-hidden="true"></i>
+			</div>
+		<div class="pre-menu">
+			<div class="menu">
+				<div class="filtro"></div>
+				<i class="fa fa-times movil"></i>
+				<a href="index.php">
+				<div class="header-Container-Logo movil">
+					<center>
+						<img  style="display:inline-block"src="img/logo-open-sport.svg" alt="">
+					</center>
+				</div>
+				</a>
+				<ul>
+					<li><a href="programacion.php">PRÓXIMOS EVENTOS</a></li>
+					<li><a href="deportes.php">DEPORTES</a></li>
+					<li><a href="">CONTRÁTALO</a></li>
+                    <li class="login user"><a href=""><i class="fa fa-user"></i><p>Manuel</p></a></li>
+				</ul>
+			</div>
+		</div>		
+		</div>
+	</header>
+	<div class="wrapper">
 <div class="preheader"></div>
 <div class="profile">
     <div class="profile-Box">
@@ -22,7 +92,18 @@
                 <div class="info pass"><p>Contraseña:********</p></div>
                 <div class="link"><a href="">Cambiar contraseña</a></div>
             </div>
-            <div class="data"></div>
+            <div class="data">
+                <div class="info pass"><p>Telefono:8218213</p></div>
+                <div class="link"><a href="">Cambiar nº de teléfono</a></div>
+            </div>
+            <div class="data">
+                <div class="info pass"><p>Contraseña:********</p></div>
+                <div class="link"><a href="">Cambiar contraseña</a></div>
+            </div>
+            <div class="data">
+                <div class="info pass"><p>Telefono:8218213</p></div>
+                <div class="link"><a href="">Cambiar nº de teléfono</a></div>
+            </div>
         </div>
     </div>
     </div>
